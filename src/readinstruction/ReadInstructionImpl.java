febdf96b437dcac38e0f119dbd82cf6a7a43d8aa
hadoth <valout13>
2017-04-20 20:34:58 +0200

    Fix typo

--- src/readinstruction/ReadInstructionImpl.java
+++ src/readinstruction/ReadInstructionImpl.java
@@ -8,12 +8,12 @@ import java.util.UUID;
 public class ReadInstructionImpl extends ReadInstruction {
     private int waitTime;
 
-    public ReadInstructionImpl(UUID instructionId, int readAddres, int arrivalTime, boolean hasDeadline) {
-        super(instructionId, readAddres, arrivalTime, hasDeadline);
+    public ReadInstructionImpl(UUID instructionId, int readAddress, int arrivalTime, boolean hasDeadline) {
+        super(instructionId, readAddress, arrivalTime, hasDeadline);
     }
 
     public int read(int actualTime){
         this.waitTime = actualTime - super.getArrivalTime();
-        return super.getReadAddres();
+        return super.getReadAddress();
     }
 }
