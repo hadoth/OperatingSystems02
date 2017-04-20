fe3ef1d5055795645bdba0155e0f47eeda9912ce
hadoth <valout13>
2017-04-20 21:09:45 +0200

    Implement FcfsScheduler class for Scheduler Interface

---
+++ src/scheduler/FcfsScheduler.java
@@ -0,0 +1,24 @@
+package scheduler;
+
+import os.OperatingSystem;
+import readinstruction.ReadInstruction;
+
+/**
+ * Created by Karol on 2017-04-20.
+ */
+public class FcfsScheduler implements Scheduler {
+    @Override
+    public void setOs(OperatingSystem parentOs) {
+
+    }
+
+    @Override
+    public void push(ReadInstruction instruction) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
