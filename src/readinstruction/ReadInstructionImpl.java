2dad620b15666e193597aa873d96a4cf2d0dce5a
hadoth <valout13>
2017-04-20 20:25:33 +0200

    Implement read method for ReadInstructionImpl

--- src/readinstruction/ReadInstructionImpl.java
+++ src/readinstruction/ReadInstructionImpl.java
@@ -6,7 +6,14 @@ import java.util.UUID;
  * Created by Karol on 2017-04-20.
  */
 public class ReadInstructionImpl extends ReadInstruction {
+    private int waitTime;
+
     public ReadInstructionImpl(UUID instructionId, int readAddres, int arrivalTime, boolean hasDeadline) {
         super(instructionId, readAddres, arrivalTime, hasDeadline);
+    }
+
+    public int read(int actualTime){
+        this.waitTime = actualTime - super.getArrivalTime();
+        return super.getReadAddres();
     }
 }
