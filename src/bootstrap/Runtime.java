bb047bb0b4b7b7db54e74db93f9b323c368167db
Karol <pokomeda.karol@gmail.com>
2017-04-21 07:21:32 +0200

    Fix parsing method

--- src/bootstrap/Runtime.java
+++ src/bootstrap/Runtime.java
@@ -1,7 +1,28 @@
 package bootstrap;
 
+import os.OperatingSystem;
+import os.OperatingSystemImpl;
+import scheduler.FcfsScheduler;
+import scheduler.Scheduler;
+import utils.Clock;
+
 /**
  * Created by Karol on 2017-04-20.
  */
 public class Runtime {
+    public static void main(String[] args) {
+        String directory = "./inputdata/";
+        String fileName = "GOOD_IMMIDIATE_FALSE";
+        String loadPath = directory + fileName;
+        Clock systemClock = new Clock();
+        Scheduler systemScheduler = new FcfsScheduler();
+        OperatingSystem myOS =
+                OperatingSystemImpl.builder()
+                        .withClock(systemClock)
+                        .withSystemScheduler(systemScheduler)
+                        .withInstructionsSource(loadPath)
+                        .withConsoleOutput()
+                        .build();
+        myOS.run();
+    }
 }
