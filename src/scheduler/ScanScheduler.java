8598d6bd4acfbe54a6ff84f325647182b1038621
Karol <pokomeda.karol@gmail.com>
2017-04-21 07:27:56 +0200

    Implement ScanScheduler

---
+++ src/scheduler/ScanScheduler.java
@@ -0,0 +1,34 @@
+package scheduler;
+
+import os.OperatingSystem;
+import readinstruction.ReadInstruction;
+
+/**
+ * Created by Karol Pokomeda on 2017-04-21.
+ */
+public class ScanScheduler implements Scheduler {
+    @Override
+    public void update(int time) {
+
+    }
+
+    @Override
+    public void setOs(OperatingSystem parentOs) {
+
+    }
+
+    @Override
+    public void push(ReadInstruction instruction) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
